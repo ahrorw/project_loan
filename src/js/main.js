49f7b4e601ef69c5d5bd4e5376889fe6ef819bdd
Akhror Sharafutdinov <rgras@mail.ru>
2022-01-03 15:44:01 +0300

    add playVideo module (YouTube API)

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 import Slider from './modules/slider';
+import VideoPlayer from './modules/playVideo';
 
 window.addEventListener('DOMContentLoaded', () => {
 'use strict';
-const slider = new Slider('.page', '.next');
+const slider = new Slider('.page', '.next'),
+    player = new VideoPlayer('.showup .play', '.overlay');
 slider.render();
+player.init();
 });
