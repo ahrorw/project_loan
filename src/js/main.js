216005a1cd3f11d749d707b6cff44d7e96461f43
Akhror Sharafutdinov <rgras@mail.ru>
2022-01-05 14:33:22 +0300

    refactoring slider module and add MainSlider module

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,11 @@
-import Slider from './modules/slider';
+import MainSlider from './modules/slider/sliderMain';
 import VideoPlayer from './modules/playVideo';
 
 window.addEventListener('DOMContentLoaded', () => {
-'use strict';
-const slider = new Slider('.page', '.next'),
-    player = new VideoPlayer('.showup .play', '.overlay');
-slider.render();
-player.init();
+    'use strict';
+    const slider = new MainSlider({btns: '.next', page: '.page'}),
+        player = new VideoPlayer('.showup .play', '.overlay');
+        
+    slider.render();
+    player.init();
 });
