3bf0bb28382d7437fa188bf896c6c7910abd59a4
Sharafutdinov Akhror <u@example.com>
2022-01-12 17:56:27 +0300

    add showInfo and Download modules

---
+++ src/js/modules/download.js
@@ -0,0 +1,23 @@
+export default class Download {
+    constructor (triggers){
+        this.btns = document.querySelectorAll(triggers);
+        this.path = 'assets/img/mainbg.jpg';
+    }
+    downloadItem(path) {
+        const element = document.createElement('a');
+        element.setAttribute('href', path);
+        element.setAttribute('download', 'nice_pic');
+        element.style.display = 'none';
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+    }
+    init() {
+        this.btns.forEach(item => {
+            item.addEventListener('click', (e) => {
+                e.stopPropagation();
+                this.downloadItem(this.path);
+            });
+        });
+    }
+}
